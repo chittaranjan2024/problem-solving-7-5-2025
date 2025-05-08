7dc74a6fbd20165a9cb1c2ba6b38728d1185b2ce
Chittaranjan <Chittaranjan@guvi.in>
2025-05-08 11:56:38 +0530

    Day-1

---
+++ Day1/src/com/test/SumOfDigits.java
@@ -0,0 +1,34 @@
+package com.test;
+
+/*
+ * 234
+ * 2+3+4=9
+ */
+
+/*
+ * 234%10=4
+ * 234/10=23%10=3
+ */
+public class SumOfDigits {
+	
+	public static int sumOfDigits(int num)
+	{
+		int rem=0,sum=0;
+		
+		for(;num>0;num=num/10)
+		{
+			rem=num%10;
+			sum=sum+rem;
+			
+		}
+		
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		
+
+		System.out.println(sumOfDigits(897));
+	}
+
+}
