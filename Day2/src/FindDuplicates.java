063b2ed3b3c1701626f003df8142bca9234a8e20
Chittaranjan <Chittaranjan@guvi.in>
2025-05-09 11:53:56 +0530

    Day-2

---
+++ Day2/src/FindDuplicates.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindDuplicates {
+	
+	public static void findDuplicate(int arr[])
+	{
+		Set<Integer> seen=new HashSet<Integer>();
+		Set<Integer> duplicates=new HashSet<Integer>();
+		
+		for(int num:arr)
+		{
+			if(!seen.add(num))
+			{
+				//If add returns false, it means num is already present
+				duplicates.add(num);
+			}
+		}
+		
+		
+		if(duplicates.isEmpty())
+		{
+			System.out.println("No repeating elements found!!");
+		}
+		else
+		{
+			System.out.println("Dplicate elements:"+duplicates);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,3,2,7,8,2,3,1};
+		findDuplicate(arr);
+		
+
+	}
+
+}
