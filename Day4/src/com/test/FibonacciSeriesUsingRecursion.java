7b43a3fdb3fc9d288802f940e5be027ccb898895
Chittaranjan <Chittaranjan@guvi.in>
2025-05-12 12:17:23 +0530

    Day-4

---
+++ Day4/src/com/test/FibonacciSeriesUsingRecursion.java
@@ -0,0 +1,32 @@
+package com.test;
+/*
+ * 0 1 1 2
+ */
+public class FibonacciSeriesUsingRecursion {
+	
+	public static int fibonacci(int n)
+	{
+		if(n==0)
+			return 0;  //base case
+		else if(n==1)
+			return 1; //base case
+		else
+			return fibonacci(n-1)+fibonacci(n-2); //recursive calls
+	}
+	
+	
+	public static void main(String args[])
+	{
+		int terms=10;
+		
+		for(int i=0;i<terms;i++)
+		{
+			System.out.print(fibonacci(i)+" ");
+		}
+		
+		
+	}
+	
+	
+
+}
